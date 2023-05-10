6c2e5f0ae43b16054b3cbb052e95603efbe31a0f
Phymon89 <aten_shun@hotmail.co.uk>
2023-05-10 12:11:10 +0100

    Section 3 amendments implemented

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,31 +4,22 @@ const db = require("../db/connection");
 const seed = require("../db/seeds/seed");
 const testData = require("../db/data/test-data/index");
 const sorted = require("jest-sorted");
+const categories = require("../db/data/test-data/categories");
 
 beforeEach(() => seed(testData));
 
 afterAll(() => db.end());
 
-describe("get welcome message", () => {
-  test("returns a status code of 200", () => {
-    return request(app).get("/api").expect(200);
-  });
-  test("should return a welcome message to the user", () => {
-    return request(app)
-      .get("/api")
-      .then(({ body }) => {
-        expect(body.msg).toEqual("Connection established");
-      });
-  });
-});
 describe("returns all categories from the GET category request", () => {
   test("returns an array of category objects, each containing a property of SLUG and a property of DESCRIPTION", () => {
     return request(app)
       .get("/api/categories")
       .expect(200)
       .then(({ body }) => {
-        expect(body.categories[0]).hasOwnProperty("slug");
-        expect(body.categories[1]).hasOwnProperty("description");
+        body.categories.forEach((categoriesObj) => {
+          expect(categoriesObj).hasOwnProperty("slug");
+          expect(categoriesObj).hasOwnProperty("description");
+        });
         expect(Array.isArray(body.categories));
       });
   });
--- controller.js
+++ controller.js
@@ -1,16 +1,10 @@
 const { fetchCategories } = require("./model");
 
-exports.getStatus = (req, res) => {
-  res
-    .status(200)
-    .send({ msg: "Connection established" })
-    .catch((err) => {
-      console.log(err, "error in controller");
-    });
-};
 exports.getCategories = (req, res, next) => {
-  fetchCategories().then((categoriesArray) => {
-    res.status(200).send({ categories: categoriesArray });
-  });
+  fetchCategories()
+    .then((categoriesArray) => {
+      res.status(200).send({ categories: categoriesArray });
+    })
+    .catch(next);
 };
 console.log("In Controller!");
