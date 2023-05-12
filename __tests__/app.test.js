8f309da9abe712a77e4a746b39792923a3022e08
Phymon89 <aten_shun@hotmail.co.uk>
2023-05-12 15:16:52 +0100

    task 5 completed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -89,24 +89,62 @@ describe("returns an review that corresponds to the review_id that is passed in"
   });
 });
 
-// describe("returns all reviews from the GET reviews request", () => {
-//   test("returns an array of review objects, each containing a property of review_id, title, review_body, designer, review_img_url, votes, category, owner and created_at", () => {
-//     return request(app)
-//       .get("/api/reviews/1")
-//       .expect(200)
-//       .then(({ body }) => {
-//         body.reviews.forEach((reviewsObj) => {
-//           expect(reviewsObj).hasOwnProperty("review_id");
-//           expect(reviewsObj).hasOwnProperty("title");
-//           expect(reviewsObj).hasOwnProperty("review_body");
-//           expect(reviewsObj).hasOwnProperty("designer");
-//           expect(reviewsObj).hasOwnProperty("review_img_url");
-//           expect(reviewsObj).hasOwnProperty("votes");
-//           expect(reviewsObj).hasOwnProperty("category");
-//           expect(reviewsObj).hasOwnProperty("owner");
-//           expect(reviewsObj).hasOwnProperty("created_at");
-//         });
-//         expect(Array.isArray(body.reviews));
-//       });
-//   });
-// });
+describe("returns all reviews from the GET reviews request", () => {
+  test("returns an array of review objects, each containing a property of review_id, title, designer, review_img_url, votes, category, owner and created_at and does not contain a property of review_body ", () => {
+    return request(app)
+      .get("/api/reviews")
+      .expect(200)
+      .then((response) => {
+        expect(response.body.reviews).toBeSortedBy("created_at");
+        response.body.reviews.forEach((reviewsObj) => {
+          expect(reviewsObj).hasOwnProperty("review_id");
+          expect(reviewsObj).hasOwnProperty("title");
+          expect(reviewsObj).not.hasOwnProperty("review_body");
+          expect(reviewsObj).hasOwnProperty("designer");
+          expect(reviewsObj).hasOwnProperty("review_img_url");
+          expect(reviewsObj).hasOwnProperty("votes");
+          expect(reviewsObj).hasOwnProperty("category");
+          expect(reviewsObj).hasOwnProperty("owner");
+          expect(reviewsObj).hasOwnProperty("created_at");
+          expect(reviewsObj).hasOwnProperty("comment_count");
+        });
+        expect(Array.isArray(response.body.reviews));
+      });
+  });
+});
+
+test("each review has the correct review_id", () => {
+  return request(app)
+    .get("/api/reviews/3")
+    .then(({ body }) => {
+      expect(body.review.review_id).toEqual(3);
+    });
+});
+test("returns with the oldest reviews first", () => {
+  return request(app)
+    .get("/api/reviews")
+    .then(({ body }) => {
+      expect([
+        body.reviews[0].created_at,
+        body.reviews[1].created_at,
+        body.reviews[2].created_at,
+        body.reviews[3].created_at,
+      ]).toBeSorted({ ascending: true });
+    });
+});
+test("gives a 404 error when a valid but non-existent path is passed in", () => {
+  return request(app)
+    .get("/api/reviews/1400")
+    .expect(404)
+    .then(({ body }) => {
+      expect(body.msg).toBe("Path not found");
+    });
+});
+test("gives a 400 error when a valid but non-existant path is passed in", () => {
+  return request(app)
+    .get("/api/reviews/nonsense")
+    .expect(400)
+    .then(({ body }) => {
+      expect(body.msg).toBe("Bad Request");
+    });
+});
--- app.js
+++ app.js
@@ -1,35 +1,42 @@
 const express = require("express");
 const app = express();
 
-const { getCategories, getEndpoints, getReviewsById } = require("./controller");
+const {
+  getCategories,
+  getEndpoints,
+  getReviewsById,
+  getReviews,
+} = require("./controller");
 
 app.get("/api/categories", getCategories);
 
 app.get("/api", getEndpoints);
 
+app.get("/api/reviews", getReviews);
+
 app.get("/api/reviews/:review_id", getReviewsById);
 
 app.use((req, res, next) => {
-  console.log("Err Handler", res.status, "Operational");
+  // console.log("Err Handler", res.status, "Operational");
   res.status(404).send({ msg: "Path not found" });
 });
 
 app.use((err, req, res, next) => {
   if (err.status && err.msg) {
-    console.log(err.status, "Err Handler 1");
+    // console.log(err.status, "Err Handler 1");
     res.status(err.status).send({ msg: err.msg });
   } else next(err);
 });
 
 app.use((err, req, res, next) => {
   if (err.code === "22P02") {
-    console.log(err.status, "Err Handler 2");
+    // console.log(err.status, "Err Handler 2");
     res.status(400).send({ msg: "Bad Request" });
   } else next(err);
 });
 
 app.use((err, req, res, next) => {
-  console.log(err, "Err Handler 3");
+  // console.log(err, "Err Handler 3");
   res.status(500).send({ msg: "Server Error" });
 });
 
